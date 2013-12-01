f71f8191131e71abee88de30833047099f6029cb
Jeff Rose <jrose0@gmail.com>
2013-11-30 19:14:31 -0500

    Moved PerceptronTest into the correct namespace since it was in the wrong one.

--- ArtificialNeuralNetwork/ArtificialNeuralNetworkTesting/Unit Tests/PerceptronTest.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetworkTesting/Unit Tests/PerceptronTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ArtificialNeuralNetwork;
 
-namespace PerceptronTesting
+namespace ArtificialNeuralNetworkTesting
 {
 	[TestClass]
 	public class PerceptronTest
